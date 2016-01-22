6fd72df2712497fedfe3f9746a474a1e465ee42d
hafuu <hafuu46@gmail.com>
2016-01-22 18:18:59 +0900

    Run tests when the file is changed

--- Persimmon.Juice/Program.fs
+++ Persimmon.Juice/Program.fs
@@ -1,3 +1,47 @@
-﻿[<EntryPoint>]
+﻿open System.IO
+open System
+open System.Reactive.Linq
+open System.Diagnostics
+
+let runTests (argv: string[]) =
+  use proc = new Process()
+  let startInfo = proc.StartInfo
+  startInfo.FileName <- @".\Persimmon.Console\Persimmon.Console.exe"
+  startInfo.Arguments <- argv |> Seq.map (fun x -> if x.StartsWith("-") then x else "\"" + x + "\"") |> String.concat " "
+  startInfo.RedirectStandardOutput <- true
+  startInfo.RedirectStandardError <- true
+  startInfo.UseShellExecute <- false
+  startInfo.CreateNoWindow <- true
+
+  Console.Clear()
+  printfn "run tests at %s." (DateTime.Now.ToString())
+  proc.Start() |> ignore
+  Console.WriteLine(proc.StandardOutput.ReadToEnd())
+  proc.WaitForExit()
+
+  printfn "press 'q' to quit or Enter to run tests."
+
+module Observable =
+  let throttle (dueTime: TimeSpan) (source: IObservable<_>) = source.Throttle(dueTime)
+
+[<EntryPoint>]
 let main argv =
-  0
+  runTests(argv)
+  
+  let args = Args.parse Args.empty (List.ofArray argv)
+  let watchFile = args.Inputs.Head
+
+  use watcher = new FileSystemWatcher(watchFile.DirectoryName, watchFile.Name)
+
+  Observable.merge watcher.Created watcher.Changed
+  |> Observable.throttle (TimeSpan.FromSeconds(1.0))
+  |> Observable.add (fun _ -> runTests argv)
+
+  watcher.EnableRaisingEvents <- true
+  
+  let rec loop() =
+    match Console.ReadKey().Key with
+    | ConsoleKey.Q -> 0
+    | ConsoleKey.Enter -> runTests argv; loop()
+    | _ -> loop()
+  loop()
